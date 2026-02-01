4f36d7adefaaae063b5e2fd662af773e0f5cf87c
Mohammed-Taha55 <mohammedtaha10683@gmail.com>
2026-02-01 22:23:42 +0530

    update-2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   theme: {
+//     extend: {},
+//   },
+// }
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-}
+  plugins: [],
+};
